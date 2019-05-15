6384398ebb57594d16bea518ad17126a9819ea81
kuter <contact@devktr.pl>
2019-05-15 22:16:03 +0200

    init

---
+++ reactjs/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <h1>Hellxlo, world!</h1>,
+  document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+var webpack = require('webpack');
+var BundleTracker = require('webpack-bundle-tracker');
+
+module.exports = {
+    mode: 'development',
+    entry: './reactjs/index.js',
+    output: {
+        path: path.resolve('./assets/webpack_bundles/'),
+        filename: "[name]-[hash].js"
+    },
+    plugins: [
+        new BundleTracker({filename: './webpack-stats.json'})
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react']
+                    }
+                }
+            }
+        ]
+    }
+};
