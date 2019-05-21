a35830cd81bb42f4109962b1dbbd1c75f03236ca
kuter <contact@devktr.pl>
2019-05-21 21:30:01 +0200

    Assignment-solution definition of done

--- reactjs/App.js
+++ reactjs/App.js
@@ -5,7 +5,16 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <h1>Hello, world !</h1>
+                <ol>
+                    <li>Create an input field (in App component) with a change listener which outputs the length of the entered text below it (e.g. in a paragraph).</li>
+                    <li>Create a new component (=> ValidationComponent) which receives the text length as a prop</li>
+                    <li>Inside the ValidationComponent, either output "Text too short" or "Text long enough" depending on the text length (e.g. take 5 as minimum length)</li>
+                    <li>Create another component (=> CharComponent) and style it as an inline box (=> display: inline-block, padding: 16px, text-aligin: center, margin: 16px, border: 1px solid black).</li>
+                    <li>Render a list of CharComponents where each Char Component receives a different letter of the entered text )in the initial input field) as a prop.</li>
+                    <li>When you click a Char Component, it should be removed from the entered text.</li>
+                </ol>
+
+                Hint: Keep in mind that JavaScript strings are basically arrays !
             </div>
         )
     };
