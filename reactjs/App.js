bdc8faa0f47f2f150e036135d588e5636956ca45
kuter <contact@devktr.pl>
2019-05-22 21:14:10 +0200

    formatting jsx files with prettier

--- reactjs/App.js
+++ reactjs/App.js
@@ -1,14 +1,14 @@
-import React, { Component} from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <h1>Hello, world !</h1>
-            </div>
-        )
-    };
-};
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello, world !</h1>
+      </div>
+    );
+  }
+}
 
 export default App;
