968f79477d66c7b16b64be0059c24e7d1fcb2639
kuter <contact@devktr.pl>
2019-06-18 19:44:56 +0200

    WIP

--- project/settings.py
+++ project/settings.py
@@ -121,7 +121,7 @@ WEBPACK_LOADER = {
     "DEFAULT": {
         "CACHE": not DEBUG,
         "BUNDLE_DIR_NAME": "webpack_bundles/",  # must end with slash
-        "STATS_FILE": os.path.join(BASE_DIR, "webpack-stats-local.json"),
+        "STATS_FILE": os.path.join(BASE_DIR, "reactjs/config/webpack-stats-prod.json"),
         "POLL_INTERVAL": 0.1,
         "TIMEOUT": None,
         "IGNORE": [r".+\.hot-update.js", r".+\.map"],
